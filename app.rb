fc21012b80a9749a3f359662672c43ad734bdaa0
Gaston Ramos <ramos.gaston@gmail.com>
2016-03-01 12:27:37 -0300

    Upload the video via ftp done.

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'cuba'
 require 'mote'
 require 'mote/render'
 require 'net/http'
+require 'net/https'
+require 'json'
 
 Cuba.plugin Mote::Render
 
@@ -14,23 +16,58 @@ module Settings
   end
 end
 
+require 'net/ftp'
+
+class Upload
+  attr_reader :p
+
+  def initialize(params, file_path)
+    @p        = JSON.parse(params)
+
+    Net::FTP.open( @p['host'], @p['user'], @p['password'] ) do |ftp|
+      ftp.put file_path, File.basename(file_path)
+    end
+  end
+end
+
 Cuba.define do
   on root do
 
     on get do
-      render 'index', client_id: ENV['CLIENT_ID'], redirect_uri: ENV['REDIRECT_URI']
+      render 'index', client_id: ENV['CLIENT_ID'], redirect_uri: ENV['REDIRECT_URL']
     end
 
     on post do
-
       on param('video') do |video|
-        uri = URI.parse('https://api.ustream.tv/channels/22083914/uploads.json?type=videoupload-ftp')
-        https         = Net::HTTP.new(uri.host, uri.port)
-        https.use_ssl = true
+        uri_str = 'https://api.ustream.tv/channels/22083914/uploads.json?type=videoupload-ftp'
 
-        req = Net::HTTP::Post.new uri.path,
-                                  {'Authorization' => "Bearer #{video['access_token']}" }
+        uri               = URI.parse uri_str
+        https             = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl     = true
+        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
+        request = Net::HTTP::Post.new uri,
+                                  {'Authorization' => "Bearer #{video['access_token']}"}
+
+        request.set_form_data({"title" => "quentin"})
+        response = https.request(request)
+
+        upload = Upload.new response.body, req['video']['file'][:tempfile].path
+
+        uri_str = "https://api.ustream.tv/channels/22083914/uploads/#{upload.p['videoId']}.json"
+
+        uri               = URI.parse uri_str
+        https             = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl     = true
+        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Put.new uri,
+                                  {'Authorization' => "Bearer #{video['access_token']}"}
+
+        request.set_form_data({"status" => "ready"})
+        response = https.request(request)
+        puts upload.p['videoId'].inspect
+        puts response.inspect
       end
 
     end
